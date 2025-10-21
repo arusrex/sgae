c699db50d2feefbac69b48f91af2dfd18d439160
arusrex <arusbr@hotmail.com>
2025-10-21 14:12:54 -0300

    local_settings

---
+++ sgae/local_settings.py
@@ -0,0 +1,19 @@
+DEBUG = False
+
+ALLOWED_HOSTS = ['helenadecastro.pythonanywhere.com']
+
+SECRET_KEY = "gvr9(q+tv%ljc0!82!$j_hbcx_vhe94%=6xpo@9(d=lz+l1o3g"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'helenadecastro$default',
+        'USER': 'helenadecastro',
+        'PASSWORD': '250907Pai#',
+        'HOST': 'helenadecastro.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+    }
+}
